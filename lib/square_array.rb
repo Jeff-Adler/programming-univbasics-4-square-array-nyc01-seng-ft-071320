10d481a41a10cfc1a421615d26a5b38dfd2c3ad0
Jeff Adler <jeffm.adler@gmail.com>
2020-06-09 18:55:18 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,4 +1,10 @@
 def square_array(array)
+  counter = 0
+  square_array = []
+  while counter < array.length do
+    square_array << array[counter] * array[counter]
+    counter += 1
+  end
 end
 
 =begin
